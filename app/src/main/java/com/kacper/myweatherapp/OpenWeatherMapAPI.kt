c54a97b32ec1dc7f5cbc3a66cc3bb9289dcf5ec7
kazaw <kacperzawadzki@gmail.com>
2020-09-22 20:47:04 +0200

    Add API

---
+++ app/src/main/java/com/kacper/myweatherapp/OpenWeatherMapAPI.kt
@@ -0,0 +1,7 @@
+package com.kacper.myweatherapp
+
+class OpenWeatherMapAPI {
+    fun getWeatherGPSDataLink(lat: Double, lon: Double): String? {
+        return "http://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$lon&appid=$API_KEY"
+    }
+}
