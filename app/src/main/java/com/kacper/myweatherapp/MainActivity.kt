ed745fb821b716ff64d90bfcb8abd806d6379837
kazaw <kacperzawadzki@gmail.com>
2020-09-22 22:13:51 +0200

    Add permission support

--- app/src/main/java/com/kacper/myweatherapp/MainActivity.kt
+++ app/src/main/java/com/kacper/myweatherapp/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.kacper.myweatherapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.Window
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
+
+    private val PERMISSIONS_REQUEST_LOCATION = 99
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
@@ -21,6 +28,28 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
 
         toolbar_main.inflateMenu(R.menu.menu_main)
         toolbar_main.setOnMenuItemClickListener(this)
+
+        setupPermissions()
+        try {
+
+        }catch (e : Exception){
+            e.printStackTrace()
+        }
+
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makePermissionsRequest()
+        }
+    }
+
+    private fun makePermissionsRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), PERMISSIONS_REQUEST_LOCATION)
     }
 
     private fun startSettingsActivity(){
