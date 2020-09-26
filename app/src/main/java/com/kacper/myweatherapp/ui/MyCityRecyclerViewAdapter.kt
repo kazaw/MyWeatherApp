63f98f0f151858ecd3a07a76fa394e853d59db2c
kazaw <kacperzawadzki@gmail.com>
2020-09-26 21:53:12 +0200

    Add button at the end of recycler view

--- app/src/main/java/com/kacper/myweatherapp/ui/MyCityRecyclerViewAdapter.kt
+++ app/src/main/java/com/kacper/myweatherapp/ui/MyCityRecyclerViewAdapter.kt
@@ -1,11 +1,14 @@
 package com.kacper.myweatherapp.ui
 
+import android.util.Log
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.kacper.myweatherapp.R
 import com.kacper.myweatherapp.data.City
 
@@ -14,33 +17,68 @@ class MyCityRecyclerViewAdapter(
     private var listener:  (City) -> Unit
 ) : RecyclerView.Adapter<MyCityRecyclerViewAdapter.ViewHolder>() {
 
+    private val ITEM_VIEW = 0
+    private val BUTTON_VIEW = 1
+    private val log_tag = "RECYCLER VIEW"
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position < values.size) {
+            ITEM_VIEW;
+        } else {
+            BUTTON_VIEW;
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.fragment_city, parent, false)
+        lateinit var view : View
+        when(viewType){
+            ITEM_VIEW -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.fragment_city, parent, false)
+            }
+            BUTTON_VIEW -> {
+                view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.add_city_row, parent, false)
+            }
+        }
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = values[position]
-        val imageResourceId = holder.itemView.context.resources.getIdentifier(
-            item.iconName, "drawable",
-            holder.itemView.context.packageName
-        )
-        holder.imageView.setImageResource(imageResourceId)
-        holder.nameView.text = item.name
-        holder.temperatureView.text = item.temperature.toString()
-        holder.itemView.setOnClickListener { listener(item) }
+        if (position >= values.size) {
+            Log.d(log_tag, "Button-Position $position")
+            holder.buttonView?.setOnClickListener {
+                onAddCityButtonClick()
+            }
+        } else {
+            Log.d(log_tag, "Item-Position $position")
+            val item = values[position]
+            val imageResourceId = holder.itemView.context.resources.getIdentifier(
+                item.iconName, "drawable",
+                holder.itemView.context.packageName
+            )
+            holder.imageView?.setImageResource(imageResourceId)
+            holder.nameView?.text = item.name
+            holder.temperatureView?.text = item.temperature.toString()
+            holder.itemView.setOnClickListener { listener(item) }
+        }
     }
 
-    override fun getItemCount(): Int = values.size
+    override fun getItemCount(): Int = values.size + 1
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView : ImageView = view.findViewById(R.id.imageView_weather_list)
-        val nameView: TextView = view.findViewById(R.id.textView_list_name)
-        val temperatureView: TextView = view.findViewById(R.id.textView_list_temperature)
+        val imageView : ImageView? = view.findViewById(R.id.imageView_weather_list)
+        val nameView: TextView? = view.findViewById(R.id.textView_list_name)
+        val temperatureView: TextView? = view.findViewById(R.id.textView_list_temperature)
+        val buttonView : Button? = view.findViewById(R.id.button_add_city)
 
         override fun toString(): String {
-            return super.toString() + " '" + temperatureView.text + "'"
+            return super.toString() + " '" + temperatureView?.text + "'"
         }
+    }
+
+    private fun onAddCityButtonClick(){
+        val addCityFragment = NewCityFragment.newInstance("TODO","TODO")
+        //supportFragmentManager.let { addCityFragment.show(context, "AddCityFragment") }
     }
 }
