25252d4176e68f78c0d8d8a697a193d5ad606de4
kazaw <kacperzawadzki@gmail.com>
2020-09-26 21:52:49 +0200

    Add NewCityFragment

--- app/src/main/java/com/kacper/myweatherapp/ui/NewCityFragment.kt
+++ app/src/main/java/com/kacper/myweatherapp/ui/NewCityFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.kacper.myweatherapp.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -12,12 +13,7 @@ import com.kacper.myweatherapp.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NewCityFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class NewCityFragment : Fragment() {
+class NewCityFragment : DialogFragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -39,15 +35,6 @@ class NewCityFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewCityFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             NewCityFragment().apply {
