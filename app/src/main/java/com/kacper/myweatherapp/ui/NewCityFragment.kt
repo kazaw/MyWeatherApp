87d8f8b17a2b48fce81427cb84ff9e6eb928dc87
kazaw <kacperzawadzki@gmail.com>
2020-09-27 22:09:06 +0200

    Work in progress button

--- app/src/main/java/com/kacper/myweatherapp/ui/NewCityFragment.kt
+++ app/src/main/java/com/kacper/myweatherapp/ui/NewCityFragment.kt
@@ -1,11 +1,15 @@
 package com.kacper.myweatherapp.ui
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.preference.PreferenceManager
 import com.kacper.myweatherapp.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -13,13 +17,15 @@ import com.kacper.myweatherapp.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class NewCityFragment : DialogFragment() {
+class NewCityFragment : DialogFragment(), View.OnClickListener {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -30,8 +36,10 @@ class NewCityFragment : DialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_city, container, false)
+        val v = inflater.inflate(R.layout.fragment_new_city, container, false)
+        val button : Button = v.findViewById(R.id.button_add_city_dialog)
+        button.setOnClickListener(this)
+        return v
     }
 
     companion object {
@@ -43,5 +51,10 @@ class NewCityFragment : DialogFragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onClick(p0: View?) {
+        Toast.makeText(context, "ADDD CITY FRAGMET", Toast.LENGTH_SHORT).show()
+        dismiss()
     }
 }
