275baf5ec93a48b11044935ef83303e0a573a8f3
kazaw <kacperzawadzki@gmail.com>
2020-09-28 21:44:01 +0200

    Add creating temperatureString for use globally

--- app/src/main/java/com/kacper/myweatherapp/utilities/TemperatureConverter.kt
+++ app/src/main/java/com/kacper/myweatherapp/utilities/TemperatureConverter.kt
@@ -1,5 +1,7 @@
 package com.kacper.myweatherapp.utilities
 
+import java.text.DecimalFormat
+
 fun convertKelvinToCelsius(kelvin : Double) : Double {
     return kelvin - 273.15
 }
@@ -13,4 +15,17 @@ fun convertCelsiusToFahrenheit(celsius : Double) : Double {
 }
 fun convertFahrenheitToKelvin(fahrenheit : Double) : Double {
     return (fahrenheit + 459.67) * (5/9)
+}
+
+fun getTemperatureString(key: String, temperature : Double) : String {
+    val decimalFormat = DecimalFormat("#.##")
+    var temperatureString = ""
+    when(key){
+        "C" -> temperatureString =
+            decimalFormat.format(convertKelvinToCelsius(temperature)).toString() + "\u2103"
+        "K" -> temperatureString = "$temperature K"
+        "F" -> temperatureString =
+            decimalFormat.format(convertKelvinToFahrenheit(temperature)).toString() + "\u2109"
+    }
+    return  temperatureString
 }
