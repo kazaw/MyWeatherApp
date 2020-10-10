dc0e3460b35e4eff57ef2db0e5c8b0772bc065da
kazaw <kacperzawadzki@gmail.com>
2020-10-10 19:46:54 +0200

    Add eventBus usage LiveData only work once

--- app/src/main/java/com/kacper/myweatherapp/MainActivity.kt
+++ app/src/main/java/com/kacper/myweatherapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -13,6 +14,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.preference.PreferenceManager
 import com.android.volley.Request
@@ -20,16 +24,20 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.kacper.myweatherapp.api.OpenWeatherMapAPI
 import com.kacper.myweatherapp.data.City
+import com.kacper.myweatherapp.events.ClickCityEvent
 import com.kacper.myweatherapp.data.WeatherAdapter
+import com.kacper.myweatherapp.events.DeleteCityEvent
+import com.kacper.myweatherapp.events.InsertCityEvent
 import com.kacper.myweatherapp.ui.CityFragment
 import com.kacper.myweatherapp.utilities.*
 import com.kacper.myweatherapp.viewmodel.CityViewModel
 import com.kacper.myweatherapp.viewmodel.CityViewModelFactory
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_weather.*
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
 import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener, SharedPreferences.OnSharedPreferenceChangeListener {
@@ -44,6 +52,7 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener, Share
     private lateinit var cityViewModel: CityViewModel
     private lateinit var cityViewModelFactory: CityViewModelFactory
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
@@ -57,10 +66,8 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener, Share
         toolbar_main.inflateMenu(R.menu.menu_main)
         toolbar_main.setOnMenuItemClickListener(this)
 
-        gson = Gson()
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         sharedPreferences.registerOnSharedPreferenceChangeListener(this)
-        sharedPreferencesEditor = sharedPreferences.edit()
         setupPermissions()
         requestQueue = Volley.newRequestQueue(this)
 
@@ -68,59 +75,66 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener, Share
         cityViewModel = ViewModelProvider(this, cityViewModelFactory).get(
             CityViewModel::class.java
         )
-
-        if (sharedPreferences.getString(KEY_PREFERENCE_CITY_LIST, null) != null){
-            getCityList()
-        } else {
-            cityList = ArrayList()
-            activeCity = City("My location", 15.3, 15.3)//TODO: get device location
-            cityList.add(activeCity)
-            val jsonString = gson.toJson(cityList)
-            sharedPreferencesEditor.putString(KEY_PREFERENCE_CITY_LIST, jsonString)
-            sharedPreferencesEditor.apply()
-        }
-        try {
-            //cityList = WeatherAdapter.getCityList() as MutableList<City>
-            getWeatherData()
-            activeCity = cityList[0]//TODO: Selected from recycler view
+        cityViewModel.getAll().observeOnce(this, {
+            Log.d("MainActivityDEBUG", "cityViewModel-1")
+            this.cityList = it as MutableList<City>
+            if (cityList.size == 0){
+                activeCity = City("My location", 15.3, 15.3)//TODO: get device location
+                cityViewModel.insert(activeCity)
+                Log.d("MainActivityDEBUG", "cityViewModel-2")
+            }
+            activeCity = cityList[0]
             setUI(activeCity)
-        }catch (e: Exception){
-            e.printStackTrace()
-        }
+            Log.d("MainActivityDEBUG", "$activeCity ${activeCity.uid}")
+            updateAll(cityList)
+        })
+
+
+
+    }
+    private fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
+        observe(lifecycleOwner, object : Observer<T> {
+            override fun onChanged(t: T?) {
+                observer.onChanged(t)
+                Log.d("MainActivityDEBUG", "removed observer")
+                removeObserver(this)
+            }
+        })
     }
 
-/*    private fun getWeatherData(lat: Double, lon: Double){
-        val url = OpenWeatherMapAPI.getWeatherGPSDataLink(lat, lon)
+    override fun onStop() {
+        super.onStop()
+        EventBus.getDefault().unregister(this)
+        Log.d("MainActivityDEBUG", "EventBus unregistered")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        EventBus.getDefault().register(this)
+        Log.d("MainActivityDEBUG", "EventBus registered")
+    }
+
+    private fun getWeatherData(city : City, isUpdate: Boolean){
+        val url = OpenWeatherMapAPI.getWeatherGPSDataLink(city.lat, city.lon)
         val stringRequest = StringRequest(
             Request.Method.GET, url,
             { response ->
-                activeCity = cityList[1]//TODO: Selected from recycler view
-                activeCity = WeatherAdapter.setCityFromJsonString(response, activeCity)
-                setUI(activeCity)
+                var tmpCity = WeatherAdapter.setCityFromJsonString(response, city)
+                if (isUpdate){
+                    Log.d("MainActivityDEBUG", "volley and update ok $tmpCity")
+                    cityViewModel.update(tmpCity)
+                }
+                else{
+                    Log.d("MainActivityDEBUG", "volley and insert ok $tmpCity")
+                    cityViewModel.insert(tmpCity)
+                }
             },
-            { Toast.makeText(this, "Network didnt work", Toast.LENGTH_SHORT).show() })
+            {
+                Toast.makeText(this, "Network didnt work", Toast.LENGTH_SHORT).show()
+                Log.d("MainActivityDEBUG", "volley didnt work $city")
+            })
         requestQueue.add(stringRequest)
-    }*/
-
-    private fun getWeatherData(){
-        for (i in 0 until cityList.size) {
-            var item = cityList[i]
-            val url = OpenWeatherMapAPI.getWeatherGPSDataLink(item.lat, item.lon)
-            val stringRequest = StringRequest(
-                Request.Method.GET, url,
-                { response ->
-
-                    item = WeatherAdapter.setCityFromJsonString(response, item)
-                    cityList[i] = item
-                    Log.d("MainActivityDEBUG", "volley ok $i $item")
-                },
-                {
-                    Toast.makeText(this, "Network didnt work $i", Toast.LENGTH_SHORT).show()
-                    Log.d("MainActivityDEBUG", "volley didnt work $i $item")})
-            requestQueue.add(stringRequest)
-        }
     }
-
 
     private fun setUI(city: City){// TODO: USE Event bus to change this onclick
         val decimalFormat = DecimalFormat("#.##")
@@ -129,13 +143,6 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener, Share
             getTemperatureString(
                 it, temperature)
         }
-/*        when(sharedPreferences.getString(KEY_PREFERENCE_TEMPERATURE, "")){
-            "C" -> temperatureString =
-                decimalFormat.format(convertKelvinToCelsius(temperature)).toString() + "\u2103"
-            "K" -> temperatureString = "$temperature K"
-            "F" -> temperatureString =
-                decimalFormat.format(convertKelvinToFahrenheit(temperature)).toString() + "\u2109"
-        }*/
         textView_location.text = city.name
         textView_longitude_data.text = decimalFormat.format(city.lon)
         textView_latitude_data.text = decimalFormat.format(city.lat)
@@ -147,6 +154,13 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener, Share
             packageName
         )
         imageView_weather_icon.setImageResource(imageResourceId)
+    }
+
+    private fun updateAll(list: List<City>){
+        for (item in list){
+            Log.d("MainActivityDEBUG", "updateAll $item")
+            getWeatherData(item, true)
+        }
     }
 
     private fun setupPermissions() {
@@ -173,17 +187,28 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener, Share
     }
 
     private fun navigationClick(){//TODO: Change name
-        //Toast.makeText(this, "TODO: Navigation Click", Toast.LENGTH_SHORT).show()
-        val cityFragment = CityFragment.newInstance(1, cityList as ArrayList<City>)
+        val cityFragment = CityFragment.newInstance(1)
         supportFragmentManager.let { cityFragment.show(it, "CityFragment") }
     }
 
-    inline fun <reified T> genericType() = object: TypeToken<T>() {}.type
-    private fun getCityList(){
-        Log.d("MainActivityDEBUG", "getCityList")
-        val jsonString: String? = sharedPreferences.getString(KEY_PREFERENCE_CITY_LIST, "")
-        val type = genericType<List<City>>()
-        cityList = gson.fromJson(jsonString, type)
+    @Subscribe
+    fun onClickCityEvent(event : ClickCityEvent){
+        Log.d("MainActivityDEBUG", "EventBus is working ${event.city}")
+        getWeatherData(event.city, true)
+        activeCity = event.city
+        setUI(activeCity)
+    }
+
+    @Subscribe
+    fun onDeleteCityEvent(event: DeleteCityEvent){
+        Log.d("MainActivityDEBUG", "Delete city event${event.city}")
+        cityViewModel.delete(event.city)
+    }
+
+    @Subscribe
+    fun onInsertCityEvent(event: InsertCityEvent){
+        Log.d("MainActivityDEBUG", "Insert city event${event.city}")
+        getWeatherData(event.city, false)
     }
 
     override fun onMenuItemClick(item: MenuItem?): Boolean {
@@ -197,13 +222,8 @@ class MainActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener, Share
     }
 
     override fun onSharedPreferenceChanged(p0: SharedPreferences?, p1: String?) {
-        //Toast.makeText(this, "Shared pre", Toast.LENGTH_SHORT).show()
         if (p1 == KEY_PREFERENCE_TEMPERATURE) {
             setUI(activeCity)//TODO: Change it because is ugly
-        } else if (p1 == KEY_PREFERENCE_CITY_LIST) {
-            Toast.makeText(this, "changed list", Toast.LENGTH_SHORT).show()
-            getCityList()
-            getWeatherData()
         }
     }
 
