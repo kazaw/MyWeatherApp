3a7400316ca04f168d720ac88722672511f35b93
kazaw <kacperzawadzki@gmail.com>
2020-10-10 17:37:13 +0200

    TODO: Use eventbus to change displayed city in weather fragment

--- app/src/main/java/com/kacper/myweatherapp/utilities/Constants.kt
+++ app/src/main/java/com/kacper/myweatherapp/utilities/Constants.kt
@@ -2,3 +2,6 @@ package com.kacper.myweatherapp.utilities
 
 const val KEY_PREFERENCE_TEMPERATURE = "key_preference_temperature"
 const val KEY_PREFERENCE_CITY_LIST = "key_preference_city_list"
+
+const val DATABASE_NAME = "app-db"
+const val DATABASE_TABLE_CITY = "city_table"
